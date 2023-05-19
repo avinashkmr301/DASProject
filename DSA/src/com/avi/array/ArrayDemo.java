7dfdd067f4a05d62094716c210c21d00f62fefa2
Avinash <Avinash@Avinash-NXM8ESI00234.bbrouter>
2023-05-19 10:38:08 +0530

    arraybasic

--- DSA/src/com/avi/array/ArrayDemo.java
+++ DSA/src/com/avi/array/ArrayDemo.java
@@ -1,8 +1,20 @@
 package com.avi.array;
 
+import java.util.Arrays;
+
 public class ArrayDemo {
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		int[] a= {2,3,4,5,6};
+		System.out.println(Arrays.toString(a));
+		
+		a[0]= 55;
+		for (int i : a) {
+			System.out.println(i);
+		}
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
 	}
 
 }
